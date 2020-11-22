7902903615aa0ecbb2a56853713651cee3d25a86
Abhishek-Gupta-lab <74490200+Abhishek-Gupta-lab@users.noreply.github.com>
2020-11-22 22:20:20 +0530

    Add files via upload

---
+++ jdoodle (3).cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+    int x,y;
+    cin>>x >>y;
+    cout<<"Before swapping" <<"/n";
+    cout<<"Valua of x:" <<x <<"/n";
+    cout<<"Value of y:" <<y <<"/n";
+    swap(x,y);
+    cout<<"After the swapping" <<"/n";
+    cout<<"Value of x:" <<x <<"/n";
+    cout<<"Value of y:" <<y <<"/n";
+}
