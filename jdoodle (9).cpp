babbf3270d5e0749b300be4e3d9ccc4d0876da16
Abhishek-Gupta-lab <74490200+Abhishek-Gupta-lab@users.noreply.github.com>
2020-11-29 21:36:55 +0530

    Add files via upload

---
+++ jdoodle (9).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+class samsung
+{
+    public:
+    double cost;
+    int slots;
+};
+int main()
+{
+    Samsung J6;
+    
+    J6.cost = 12500;
+    J6.cost = 2;
+    
+    cout<< "cost of Samsung J6 : " << J6.cost << end1;
+    cout<<"Number of the card slots for Samsung J6 : " << J6.slots << end1;
+    
+    return 0;
+}
