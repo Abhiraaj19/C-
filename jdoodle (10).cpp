3cbe19d4955eb4882230384ee541f3450a2768b6
Abhishek-Gupta-lab <74490200+Abhishek-Gupta-lab@users.noreply.github.com>
2020-11-29 21:35:58 +0530

    Add files via upload

---
+++ jdoodle (10).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <conio.h>
+
+using namespace std;
+
+class peacock;
+{
+    public:
+    void peacock()
+    {
+        cout << "\n National bird of India is peacock "
+    }
+};
+int main()
+{
+    Peacock p;
+    p.peacock();
+    return 0;
+}
